119721015166abdf3a04dac780da00700df00363
KuroGuo <413298956@qq.com>
2015-01-20 21:45:11 +0800

    init

--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-
-var uglify = require('gulp-uglify');
-
-gulp.task('default', function () {
-  gulp.src('./src/k-drag.js')
-    .pipe(uglify())
-    .pipe(gulp.dest('./dist/'));
-});
